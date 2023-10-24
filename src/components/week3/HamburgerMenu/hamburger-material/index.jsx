307bec364c133cded60efe4741bc6bedeba65143
Artefilex <baris.tncdmr@gmail.com>
2023-10-24 20:53:02 +0300

    updated api

--- src/components/week3/HamburgerMenu/hamburger-material/index.jsx
+++ src/components/week3/HamburgerMenu/hamburger-material/index.jsx
@@ -41,7 +41,7 @@ export default function HamburgerMaterial ({selectedIngredients}) {
         </ul>
 
         
-          {  totalPrice > 0 && (
+          {  totalPrice > 0 && convert && (
             <div className="flex flex-col gap-2 border-none outline-none"> 
             <span>Toplam Tutar :{totalPrice} TL</span>
             <select className="p-2.5  bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500  w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-[1.6rem]"> 
